962e43461976249f41bcb78db4efe24e24b655f8
jianghang8421 <jianghang8421@163.com>
2018-10-23 14:09:22 +0800

    modify ingress build script

--- ingress-nginx/build_ingress_nginx.sh
+++ ingress-nginx/build_ingress_nginx.sh
@@ -5,4 +5,4 @@ SCRIPT_ROOT=$(dirname ${BASH_SOURCE})/..
 CODE_PKG=${CODE_PKG:-${SCRIPT_ROOT}/vendor/github.com/rancher/ingress-nginx}
 
 cd ${CODE_PKG}
-CROSS=1 make
+CROSS=1 OS_ARCH_ARG[linux]="arm64" make
