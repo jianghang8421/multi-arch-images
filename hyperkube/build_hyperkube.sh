3469ba34a09c661301558781200bb91ab871873e
jianghang8421 <jianghang8421@163.com>
2018-10-23 11:48:42 +0800

    modify script

--- hyperkube/build_hyperkube.sh
+++ hyperkube/build_hyperkube.sh
@@ -5,4 +5,4 @@ SCRIPT_ROOT=$(dirname ${BASH_SOURCE})/..
 CODE_PKG=${CODE_PKG:-${SCRIPT_ROOT}/vendor/github.com/rancher/hyperkube}
 
 cd ${CODE_PKG}
-docker build -f Dockerfile.arm64 rancher/hyperkube:v1.11.3-rancher1-arm64 .
+docker build -f Dockerfile.arm64 -t rancher/hyperkube:v1.11.3-rancher1-arm64 .
